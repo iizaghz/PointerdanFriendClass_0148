cc1fa856a036b3a1e9ab493edbe6995e553ea09f
iizaghz <sukmahawaiza@gmail.com>
2025-05-06 19:37:26 +0700

    add pointer objek

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -10,3 +10,13 @@ class mahasiswa
         cout << "No induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa{1}; // pointer objek
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
+    
