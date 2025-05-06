3c0f3dc4ab5ccd848cdaf19a199c2057d796f19d
iizaghz <sukmahawaiza@gmail.com>
2025-05-06 19:41:12 +0700

    add setNama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -9,4 +9,15 @@ class mahasiswa
         friend void setNama(mahasiswa &a, string);
 };
 
+void setNama (mahasiswa &a, string b)
+    {
+    a.nama = b;
+    cout << a.nama;
+}
 
+int main()
+{
+    mahasiswa joko;
+    setNama (joko, "Kairi Kumar");
+    return 0;
+}
