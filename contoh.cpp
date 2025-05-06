d400299e50dce3fef6c8e4597ad15e377b83d99c
iizaghz <sukmahawaiza@gmail.com>
2025-05-06 18:29:47 +0700

    membuat input panjang dan lebar

--- contoh.cpp
+++ contoh.cpp
@@ -6,5 +6,12 @@ class PersegiPanjang
     public :
     int panjang, lebar;
 
-
+    public :
+    void input()
+    {
+        cout << "Masukkan panjang :";
+        cin >> panjang;
+        cout << "Masukkan lebar :" ;
+        cin >> lebar;
+    }
 }
